6aaae7867bcf2c019ced4e0ff5fef9bf87e89e7a
Natalia-Dobranova <104311088+Natalia-Dobranova@users.noreply.github.com>
2022-05-08 19:53:20 +0200

    Add files via upload

---
+++ Phone.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Homework41
+{
+    abstract class Phone
+    {
+        public abstract void Call();
+        public abstract void SendSms();
+        public abstract void TurnOff();
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Happy to see you!");
+
+        }
+     
+    }
+
+    class Android : Phone
+    {
+        public override void Call()
+        {
+            Console.WriteLine("Call on Android");
+        }
+
+        public override void SendSms()
+        {
+            Console.WriteLine("sms from Android");
+        }
+
+        public override void TurnOff()
+        {
+            Console.WriteLine("Good night! Your Android");
+        }
+    }
+
+    class IPhone : Phone
+    {
+        public override void Call()
+        {
+            Console.WriteLine("Call on iPhone");
+        }
+
+        public override void SendSms()
+        {
+            Console.WriteLine("sms from iPhone");
+        }
+
+        public override void TurnOff()
+        {
+            Console.WriteLine("I don't want to sleep, your iPhone");
+        }
+    }
+
+}
---
+++ Program_interface.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Homework42
+{
+    interface IPhone
+    {
+        void Call();
+        void SendSms();
+        void TurnOff();
+
+    }
+
+
+    class Android : IPhone
+    {
+        public void Call()
+        {
+            Console.WriteLine("Call on Android");
+        }
+
+        public void SendSms()
+        {
+            Console.WriteLine("sms from Android");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Good night! Your Android");
+        }
+    }
+
+    class Iphone : IPhone
+    {
+        public void Call()
+        {
+            Console.WriteLine("Call on iPhone");
+        }
+
+        public void SendSms()
+        {
+            Console.WriteLine("sms from iPhone");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("I don't want to sleep, your iPhone");
+        }
+    }
+
+    internal class Program2
+    {
+
+        static void Main(string[] args)
+        {
+            Android phone1 = new Android();
+            phone1.Call();
+            phone1.SendSms();
+            phone1.TurnOff();
+
+            Iphone phone2 = new Iphone();
+            phone2.Call();
+            phone2.SendSms();
+            phone2.TurnOff();
+
+            Console.ReadLine();
+
+        }
+    }
+}
